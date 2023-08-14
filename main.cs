5bef51bbfecb1ae8231c0058dbf06c0df9081978
orange-hwang <73291464+orange-hwang@users.noreply.github.com>
2023-08-14 12:37:57 +0900

    Testing for C#

---
+++ main.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ConsoleWrite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("무엇이 무엇이");
+            Console.Write("문제일까?");
+            Console.Write("\" ");
+        }
+    }
+}
+
